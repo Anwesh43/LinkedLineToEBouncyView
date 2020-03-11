7fefe67536fb1b585f443360c2754142b4ca0b25
Anwesh43 <anweshthecool0@gmail.com>
2020-03-11 10:52:03 +0530

    adding constants and libraries

--- linetoebouncyview/src/main/java/com/anwesh/uiprojects/linetoebouncyview/LineToEBouncyView.kt
+++ linetoebouncyview/src/main/java/com/anwesh/uiprojects/linetoebouncyview/LineToEBouncyView.kt
@@ -3,3 +3,19 @@ package com.anwesh.uiprojects.linetoebouncyview
 /**
  * Created by anweshmishra on 11/03/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val lines : Int = 5
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4CAF50")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
