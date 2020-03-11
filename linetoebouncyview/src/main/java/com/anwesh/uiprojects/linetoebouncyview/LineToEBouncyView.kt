e854b64ea61c45ddc0df25791b793e66a60a55d2
Anwesh43 <anweshthecool0@gmail.com>
2020-03-11 11:28:44 +0530

    adding LTEBNode class

--- linetoebouncyview/src/main/java/com/anwesh/uiprojects/linetoebouncyview/LineToEBouncyView.kt
+++ linetoebouncyview/src/main/java/com/anwesh/uiprojects/linetoebouncyview/LineToEBouncyView.kt
@@ -151,4 +151,28 @@ class LineToEBouncyView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class LineToEBouncy(var i : Int) {
+
+        private val root : LTEBNode = LTEBNode(0)
+        private var curr : LTEBNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
