dcd2f36d9d5453b6e925f37d8103a946f82e4135
Anwesh43 <anweshthecool0@gmail.com>
2020-03-11 11:02:39 +0530

    adding Canvas extension method

--- linetoebouncyview/src/main/java/com/anwesh/uiprojects/linetoebouncyview/LineToEBouncyView.kt
+++ linetoebouncyview/src/main/java/com/anwesh/uiprojects/linetoebouncyview/LineToEBouncyView.kt
@@ -24,3 +24,29 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawBouncyLineToE(scale : Float, size : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    drawLine(0f, -size, 0f, size, paint)
+    drawLine(0f, 0f, size / 2, 0f, paint)
+    for (j in 0..1) {
+        val sfi : Float = sf.divideScale(j, lines)
+        save()
+        scale(1f, 1f - 2 * j)
+        drawLine(0f, 0f, size / 2 + (size / 2) * sfi, size * sfi, paint)
+        restore()
+    }
+}
+
+fun Canvas.drawBLENode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    paint.color = foreColor
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawBouncyLineToE(scale, gap / sizeFactor, paint)
+    restore()
+}
