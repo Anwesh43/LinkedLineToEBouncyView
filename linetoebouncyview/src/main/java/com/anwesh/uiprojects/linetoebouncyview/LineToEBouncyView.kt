344adc8debe6f907ce9fdc19051f2bfe1154c53b
Anwesh43 <anweshthecool0@gmail.com>
2020-03-11 11:05:34 +0530

    adding State class

--- linetoebouncyview/src/main/java/com/anwesh/uiprojects/linetoebouncyview/LineToEBouncyView.kt
+++ linetoebouncyview/src/main/java/com/anwesh/uiprojects/linetoebouncyview/LineToEBouncyView.kt
@@ -16,6 +16,7 @@ val nodes : Int = 5
 val lines : Int = 5
 val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
+val scGap : Float = 0.02f
 val foreColor : Int = Color.parseColor("#4CAF50")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val delay : Long = 20
@@ -64,5 +65,25 @@ class LineToEBouncyView(ctx : Context) : View(ctx) {
             }
         }
         return true
+    }
+
+    data class State(var scale : Float = 0f, var dir : Float = 0f, var prevScale : Float = 0f) {
+
+        fun update(cb : (Float) -> Unit) {
+            scale += scGap * dir
+            if (Math.abs(scale - prevScale) > 1) {
+                scale = prevScale + dir
+                dir = 0f
+                prevScale = scale
+                cb(prevScale)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            if (dir == 0f) {
+                dir = 1f - 2 * prevScale
+                cb()
+            }
+        }
     }
 }
